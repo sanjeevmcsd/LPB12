4c51715ee29dd8e36af5a1476cf310bedff90b1c
SANJEEVKUMAR <SANJEEVKUMAR@9.81.223.8>
2019-11-21 09:11:29 +0530

    login page code

--- final-framework-testng/tests/com/training/sanity/tests/LoginTests.java
+++ final-framework-testng/tests/com/training/sanity/tests/LoginTests.java
@@ -19,8 +19,9 @@ import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.interactions.Actions;
 
-public class LoginTests {
-
+public class LoginTests 
+{
+	
 	private WebDriver driver;
 	private String baseUrl;
 	private LoginPOM loginPOM;
@@ -29,14 +30,16 @@ public class LoginTests {
 	
 
 	@BeforeClass
-	public static void setUpBeforeClass() throws IOException {
+	public static void setUpBeforeClass() throws IOException 
+	{
 		properties = new Properties();
 		FileInputStream inStream = new FileInputStream("./resources/others.properties");
 		properties.load(inStream);
 	}
 
 	@BeforeMethod
-	public void setUp() throws Exception {
+	public void setUp() throws Exception 
+	{
 		driver = DriverFactory.getDriver(DriverNames.CHROME);
 		loginPOM = new LoginPOM(driver); 
 		baseUrl = properties.getProperty("baseURL");
@@ -44,25 +47,50 @@ public class LoginTests {
 		screenShot = new ScreenShot(driver); 
 		// open the browser 
 		
+		
 	}
 	
 	@AfterMethod
-	public void tearDown() throws Exception {
+	public void tearDown() throws Exception 
+	{
 		Thread.sleep(1000);
-		driver.quit();
+	driver.quit();
 	}
 	@Test
-	public void validLoginTest() {
+	public void validLoginTest() throws InterruptedException 
+	{
+		String str;
 		loginPOM.sendUserName("administrator");
 		loginPOM.sendPassword("administrator@123");
 		loginPOM.clickLoginBtn(); 
-//		actions.moveToElement(target).perform()
-		driver.findElement(By.xpath("//*[@class='avatar avatar-26 photo']")).click();
+	//	actions.moveToElement(target).perform();
+	driver.findElement(By.xpath("//*[@class='avatar avatar-26 photo']")).click();
 		driver.findElement(By.id("first_name")).clear();
 		driver.findElement(By.id("first_name")).sendKeys("manzoor");
 		driver.findElement(By.id("phone")).clear();
 		driver.findElement(By.id("phone")).sendKeys("9876543210");
-		driver.findElement(By.id("submit")).click();
+		System.out.println(driver.findElement(By.xpath("//*[@class='button wp-generate-pw hide-if-no-js']")).getText());
+		
+		driver.findElement(By.xpath("//*[@class='button wp-generate-pw hide-if-no-js']")).click();
+		
+		driver.findElement(By.xpath("//*[@id='pass1-text']")).clear();
+		driver.findElement(By.xpath("//*[@id='pass1-text']")).sendKeys("Sanjusanju@321");
+		//driver.wait();
+		
+		str = driver.findElement(By.xpath("//*[@class='strong']")).getText();
+		System.out.println(str);
+	if (str.contains("Strong"))
+		{
+		System.out.println("Clicking on button");
+			driver.findElement(By.id("submit")).click();
+		}
+		else
+		{
+			System.out.println("password is not strong");
+		}
+		
+		
+		
 		//driver.findElement(By.cssSelector("class='avatar avatar-26 photo'")).click();	
 		screenShot.captureScreenShot("First");
 	}
